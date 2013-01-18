6f513743977555ee011198745fbad44b7bb5c3d9
jpd4nt <jdrawneek@nationaltheatre.org.uk>
2013-01-18 22:33:42 +0000

    add chef-client on boot

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,17 @@ include_recipe "cron"
 
 cron_d "chef-client" do
   minute "*/15"
-  command "chef-client -c /etc/chef/client.rb -j /etc/chef/dna.json"
+  command "chef-client -c /etc/chef/client.rb"
   user "root"
+end
+
+cookbook_file '/etc/init.d/chef_client' do
+  source "chef_client"
+  owner  "root"
+  group  "root"
+  mode   "0755"
+  variables()
+end
+service "chef_client" do
+  action [ :enable, :start ]
 end
